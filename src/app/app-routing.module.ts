329000d8fc6c1832826e224ae7a07e01a65ed0dd
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-10 13:38:02 +0100

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
-import { AuthGuard, UserToken, Permissions } from './guards/auth-guard.service';
+import { AuthGuard } from './guards/auth-guard.service';
 
 // Load a component for each route.
 const routes: Routes = [
   { path: 'one', component: OneComponent, canActivate: [AuthGuard] },
-  { path: 'two', component: TwoComponent },
+  { path: 'two', component: TwoComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   // Creates and configures a module with all the router providers and directives.
   imports: [RouterModule.forRoot(routes)],
   // Providers guard services
-  providers: [AuthGuard, UserToken, Permissions],
+  providers: [AuthGuard],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,29 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs/internal/Rx';
-
-export class UserToken {}
-
-export class Permissions {
-  canActivate(user: any, routeId: string): boolean {
-    return true;
-  }
-}
+import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private permissions: Permissions,
-    private currentUser: UserToken
-  ) {}
+  constructor() {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return this.permissions.canActivate(this.currentUser, route.params['id']);
+  canActivate(route: ActivatedRouteSnapshot) {
+    // enable route one
+    if (route?.routeConfig?.path == 'one') {
+      return true;
+    } else {
+      // Block route two
+      return false;
+    }
   }
 }
